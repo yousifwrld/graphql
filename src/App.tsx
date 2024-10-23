01c8918799bd20894f16d691d168be0c67fe22bb
yalsayya <yousif.alsayyad2005@gmail.com>
2024-10-23 10:49:05 +0300

    back to old

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/pages/Dashboard";
 import NotFound from "./components/pages/NotFound";
