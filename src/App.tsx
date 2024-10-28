d8639d0ce8fd591459c1c79c0a2f34926f3fd9e4
yalsayya <yousif.alsayyad2005@gmail.com>
2024-10-28 19:30:00 +0300

    basename added

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,12 @@ import Dashboard from "./components/pages/Dashboard";
 import NotFound from "./components/pages/NotFound";
 
 function App() {
+  const isLocalhost =
+    window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.1";
+  const basename = isLocalhost ? "/" : process.env.PUBLIC_URL || "/";
   return (
-    <Router>
+    <Router basename={basename}>
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
