341b311cf96542c49f7520863f2290c812c60412
yalsayya <yousif.alsayyad2005@gmail.com>
2024-10-28 19:37:30 +0300

    back to hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/pages/Dashboard";
 import NotFound from "./components/pages/NotFound";
 
 function App() {
-  const isLocalhost =
-    window.location.hostname === "localhost" ||
-    window.location.hostname === "127.0.0.1";
-  const basename = isLocalhost ? "/" : process.env.PUBLIC_URL || "/";
   return (
-    <Router basename={basename}>
+    <Router>
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
